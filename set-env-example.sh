ab529d69e2947fba01ac09c4f9606ec73accc36b
Andrew Chilton <andychilton@gmail.com>
2016-12-29 23:55:57 +1300

    Add an example env var script

---
+++ set-env-example.sh
@@ -0,0 +1,22 @@
+## --------------------------------------------------------------------------------------------------------------------
+#
+# These are the environment variables you need to pass to your server for configuration. All values are required except
+# the social keys/secrets which are optional. The values here are fake, don't try and use them! :)
+#
+## --------------------------------------------------------------------------------------------------------------------
+
+ # General
+ export DAFFY_PORT=8080
+ export DAFFY_BASE_URL=http://localhost:8080
+
+ # OAuth
+ export DAFFY_TWITTER_CONSUMER_KEY=Atljdwqfa6jXMQ9HMaiqc480a
+ export DAFFY_TWITTER_CONSUMER_SECRET=wtxuzcK4fsQQcr6pktcLStnH6RiKUZ81mUODLXfFug6uzyBNFt
+
+ # Sessions - generated with `pwgen -s 32 1`
+ export DAFFY_SESSION_AUTH_KEY_V2=IppHcUPiJnwy8Hk5sv9m6qVFS3TP0gWq
+ export DAFFY_SESSION_ENC_KEY_V2=PtwVsDVy168bBwRLSuV432s25E3ivEdy
+ export DAFFY_SESSION_AUTH_KEY_V1=8svrXLgygeaG0nQ8GM6EAj3EzNMCfP6H
+ export DAFFY_SESSION_ENC_KEY_V1=CXZnNBDa4LP82dA2iXkivS50EUvMfweA
+
+## --------------------------------------------------------------------------------------------------------------------
