20ee0e00be198497ddbc0dc20b3821c2cfc4f4d9
Andrew Chilton <andychilton@gmail.com>
2017-05-05 10:42:34 +1200

    Just pass the templates in, don't load them in init() here

--- src/internal/handlers/render.go
+++ src/internal/handlers/render.go
@@ -3,20 +3,8 @@ package handlers
 import (
 	"bytes"
 	"html/template"
-	"log"
 	"net/http"
 )
-
-var tmpl *template.Template
-
-func init() {
-	// load up all templates
-	var err error
-	tmpl, err = template.New("").ParseGlob("./templates/*.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 func render(w http.ResponseWriter, tmpl *template.Template, tmplName string, data interface{}) {
 	buf := &bytes.Buffer{}
